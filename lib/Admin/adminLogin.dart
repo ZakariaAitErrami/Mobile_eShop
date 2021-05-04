b8f51f7300dd56bcfd04bdaa2b661ee0f4c38852
ZakariaAitErrami <zakaria.ait.errami00@gmail.com>
2021-05-04 22:42:40 +0000

    Admin login

--- lib/Admin/adminLogin.dart
+++ lib/Admin/adminLogin.dart
@@ -11,7 +11,28 @@ import 'package:flutter/material.dart';
 class AdminSignInPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        flexibleSpace: Container(
+          decoration: new BoxDecoration(
+            gradient: new LinearGradient(
+              colors: [Colors.pink, Colors.lightGreenAccent],
+              begin: const FractionalOffset(0.0, 0.0),
+              end: const FractionalOffset(1.0, 0.0),
+              stops: [0.0, 1.0],
+              tileMode: TileMode.clamp,
+            ),
+          ),
+        ),
+        title: Text(
+          "e-shop",
+          style: TextStyle(
+              fontSize: 55.0, color: Colors.white, fontFamily: "Signatra"),
+        ),
+        centerTitle: true,
+      ),
+      body: AdminSignInScreen(),
+    );
   }
 }
 
@@ -23,9 +44,138 @@ class AdminSignInScreen extends StatefulWidget {
 
 class _AdminSignInScreenState extends State<AdminSignInScreen>
 {
+  final TextEditingController _passwordTextEditingController = TextEditingController(); // _means private variable
+  final TextEditingController _adminIDTextEditingController = TextEditingController();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
 
-    return SingleChildScrollView();
+    double _screenWidth = MediaQuery.of(context).size.width,
+        _screenHeight = MediaQuery.of(context).size.height;
+
+    return SingleChildScrollView(
+      child: Container(
+        decoration: new BoxDecoration(
+          gradient: new LinearGradient(
+            colors: [Colors.pink, Colors.lightGreenAccent],
+            begin: const FractionalOffset(0.0, 0.0),
+            end: const FractionalOffset(1.0, 0.0),
+            stops: [0.0, 1.0],
+            tileMode: TileMode.clamp,
+          ),
+        ),
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            Container(
+              child: Image.asset(
+                "images/admin.png",
+                height: 240.0,
+                width: 240.0,
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Text(
+                "Admin",
+                style: TextStyle(color: Colors.white, fontSize: 28.0, fontWeight: FontWeight.bold),
+              ),
+            ),
+            Form(
+              key: _formKey,
+              child: Column(
+                children: [
+                  CustomTextField(
+                    // class in widgets package
+                    controller: _adminIDTextEditingController,
+                    data: Icons.person,
+                    hintText: "Admin username",
+                    isObsecure: false,
+                  ),
+                  CustomTextField(
+                    // class in widgets package
+                    controller: _passwordTextEditingController,
+                    data: Icons.vpn_key,
+                    hintText: "Password",
+                    isObsecure: true,
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 25.0,
+            ),
+            RaisedButton(
+              onPressed: () {
+                _adminIDTextEditingController.text.isNotEmpty &&
+                    _passwordTextEditingController.text.isNotEmpty
+                    ? loginAdmin()
+                    : showDialog(
+                    context: context,
+                    builder: (c) {
+                      return ErrorAlertDialog(
+                        message: "Please write your email and password",
+                      );
+                    });
+              },
+              color: Colors.pink,
+              child: Text("Login",
+                  style: TextStyle(
+                    color: Colors.white,
+                  )),
+            ),
+            SizedBox(
+              height: 50.0,
+            ),
+            //adding horizontal line under the button
+            Container(
+              height: 4.0,
+              width: _screenWidth * 0.8,
+              color: Colors.pink,
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            FlatButton.icon(
+              onPressed: () => Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => AuthenticScreen())),
+              icon: (Icon(
+                Icons.nature_people,
+                color: Colors.pink,
+              )),
+              label: Text(
+                "I'm not Admin",
+                style:
+                TextStyle(color: Colors.pink, fontWeight: FontWeight.bold),
+              ),
+            ),
+            SizedBox(
+              height: 50.0,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+  loginAdmin(){
+     Firestore.instance.collection("admins").getDocuments().then((snapshot){
+        snapshot.documents.forEach((result) {
+          if(result.data["id"] != _adminIDTextEditingController.text.trim()){
+              Scaffold.of(context).showSnackBar(SnackBar(content: Text("The admin username is not correct"),));
+          }
+            else if(result.data["password"] != _passwordTextEditingController.text.trim()){
+              Scaffold.of(context).showSnackBar(SnackBar(content: Text("your password is not correct"),));
+            }
+            else{
+              Scaffold.of(context).showSnackBar(SnackBar(content: Text("Welcome Dear Admin "+result.data["name"]),));
+              setState(() {
+                _adminIDTextEditingController.text = "";
+                _passwordTextEditingController.text= "";
+              });
+              Route route = MaterialPageRoute(builder: (c) => UploadPage());
+              Navigator.pushReplacement(context, route);
+            }
+        });
+     });
   }
 }
